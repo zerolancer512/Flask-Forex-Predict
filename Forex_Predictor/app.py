ad7adaa14ffcbedea4fa6784cf99ac3d6c30b834
zerolancer512 <42534763+zerolancer512@users.noreply.github.com>
2020-05-19 13:53:25 +0700

    Add files via upload

--- Forex_Predictor/app.py
+++ Forex_Predictor/app.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
-from flask import Flask, request, jsonify, render_template, make_response
+from flask import Flask, request, jsonify, render_template, send_file, abort
 import pickle
 import pandas as pd
 import os
@@ -10,12 +10,21 @@ dirpath = os.getcwd()
 app = Flask(__name__)
 model = pickle.load(open(dirpath + '\\Forex_Predictor\\model\\model.pkl', 'rb'))
 
+
+
 @app.route('/')
 def home():
     '''
     For rendering results on HTML GUI
     '''
     return render_template('index.html')
+
+@app.route('/download/<path:filename>', methods = ['GET', 'POST'])
+def download(filename):
+    path = dirpath + '\\{}'.format(filename)
+    return send_file(path, as_attachment=True)
+
+
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -35,12 +44,39 @@ def predict():
 def preprocessing():
     file = request.files['datafile']
     if not file:
-        text = 'No file selected'
-    df = pd.read_csv(file)
-    df['return_1'] = df['return_2'] - df['lag_return_1']
-    df = df.to_html(dirpath+ '\\Forex_Predictor\\templates\\Pre-Processed_Data.html')
-    text = 'preprocessed success'
-    return render_template('Pre-Processed_Data.html')
+        return render_template('index.html', text = 'No file selected')
+    else:
+        df = pd.read_csv(file)
+        df['return_1'] = df['return_2'] - df['lag_return_1']
+        df.to_csv('Preprocessed_Data.csv', index = False)
+        render_template('index.html', text = 'Pre - processing completed')
+        return download('Preprocessed_Data.csv')
+        
+        
+@app.route('/FeatureSelection', methods = ['GET', 'POST'])
+def FeatureSelection():
+    file = request.files['datafile']
+    if not file:
+        return render_template('index.html', text = 'No file selected')
+    else:
+        df = pd.read_csv(file)
+        df = df[['Close', 'High', 'Low', 'return_1', 'return_2', 'return_3']]
+        df.to_csv('FeatureSelected_Data.csv', index  = False)
+        render_template('index.html', text = 'Feature Selection completed')
+        return download('FeatureSelected_Data.csv')
+
+@app.route('/Classification', methods = ['POST', 'GET'])
+def Classification():
+    file = request.files['datafile']
+    if not file:
+        return render_template('index.html', text = 'No file selected')
+    else:
+        df = pd.read_csv(file)
+        df['Up_down_predict'] = model.predict(df.values)
+        df.to_csv('Predicted_Data.csv', index = False)
+        render_template('index.html', text = 'Classification completed')
+        return download('Predicted_Data.csv')
+
 
 
 
